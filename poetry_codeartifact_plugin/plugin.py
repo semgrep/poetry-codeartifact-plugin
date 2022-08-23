de2a615c0186cceb3c286a6259689b94cb5dcf3a
Tom Petr <tom@r2c.dev>
2022-08-23 16:20:28 -0400

    fix

--- poetry_codeartifact_plugin/plugin.py
+++ poetry_codeartifact_plugin/plugin.py
@@ -30,28 +30,27 @@ def monkeypatch_authenticator(io: IO):
             netloc = urlparse(response.url)[1]
             if m := RE_CODEARTIFACT_NETLOC.match(netloc):
                 domain, domain_owner = m.groups()
-                config = self.get_repository_config_for_url(url)
-
-                io.write_line(
-                    f"Getting new CodeArtifact authorization token for repo {config.name} ({domain=}, {domain_owner=})..."
-                )
-                try:
-                    response = boto3.client("codeartifact").get_authorization_token(
-                        domain=domain,
-                        domainOwner=domain_owner,
+                if config := self.get_repository_config_for_url(url):
+                    io.write_line(
+                        f"Getting new CodeArtifact authorization token for repo {config.name} ({domain=}, {domain_owner=})..."
                     )
-                except BotoCoreError as err:
-                    raise PoetryException(
-                        f"Failed to get a new CodeArtifact authorization token: {err}\n\n-> Are your local AWS credentials up-to-date?"
+                    try:
+                        response = boto3.client("codeartifact").get_authorization_token(
+                            domain=domain,
+                            domainOwner=domain_owner,
+                        )
+                    except BotoCoreError as err:
+                        raise PoetryException(
+                            f"Failed to get a new CodeArtifact authorization token: {err}\n\n-> Are your local AWS credentials up-to-date?"
+                        )
+                    self._password_manager.set_http_password(
+                        config.name, "aws", response["authorizationToken"]
                     )
-                self._password_manager.set_http_password(
-                    config.name, "aws", response["authorizationToken"]
-                )
-                self.reset_credentials_cache()
-                self._password_manager._config = Config.create(reload=True)
+                    self.reset_credentials_cache()
+                    self._password_manager._config = Config.create(reload=True)
 
-                # Retry the request now that we're authenticated
-                return old_request(self, method, url, *args, **kwargs)
+                    # Retry the request now that we're authenticated
+                    return old_request(self, method, url, *args, **kwargs)
 
         if raise_for_status:
             response.raise_for_status()
